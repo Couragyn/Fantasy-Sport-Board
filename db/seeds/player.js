274b1355524c4c5979286b0ff143d21ab7dc8a48
max-bergen <maxbergen1234@gmail.com>
2017-06-03 22:56:01 +0000

    added migrations

--- db/seeds/player.js
+++ db/seeds/player.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   return knex('player').del()
     .then(function () {
       return Promise.all([
-        knex('player').insert({id: 1, name: 'Max', position: 'TE', team: 'The Chillers', std_adp: 1, idp_adp: 1, ppr_adp: 1}),
+        knex('player').insert({id: 1, name: 'Max', position: 'TE', team: 'The Chillers', rookie: true, std_adp: 1, idp_adp: 1, ppr_adp: 1, position_adp: 1}),
       ]);
     });
 };
