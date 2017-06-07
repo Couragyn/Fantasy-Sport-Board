82385e357fe282758a56e217bcc15f0390c0478e
Couragyn Chretien <couragyn@gmail.com>
2017-06-07 04:31:58 +0000

    provided clarity by adding comments

--- db/dbFunc/createDraftPicks.js
+++ db/dbFunc/createDraftPicks.js
@@ -16,8 +16,8 @@ module.exports = function createDraft(draftID, knex) {
               })
             }
           }
+           // If it is a snake draft, it will assign picks according to draft order on odd rounds, and reverse order on even rounds
         } else if (rows[0].draft_type === 'Snake') {
-          // If it is a snake draft, it will assign picks according to draft order on odd rounds, and reverse order on even rounds
           for (let round = 1; round <= rows[0].rounds; round += 2) {
             for(let pick = 1; pick <= rows.length; pick++) {
               knex('draft_picks').insert({draft_id: draftID, round: round, pick: pick, team_id: rows[pick-1].id})
@@ -27,7 +27,7 @@ module.exports = function createDraft(draftID, knex) {
             }
           }
           for (let round = 2; round <= rows[0].rounds; round += 2) {
-            // Keeps track of the teams in draft position, while the loop goes through backwards
+            // Keeps track of the teams in draft position, while the loop goes through backwards. Resets each round
             let team = 0;
             for(let pick = rows.length; pick >= 1; pick--) {
               knex('draft_picks').insert({draft_id: draftID, round: round, pick: pick, team_id: rows[pick-1].id})
