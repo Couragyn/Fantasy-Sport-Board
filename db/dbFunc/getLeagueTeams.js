808cc90402318f0d68201ec1ee6de740fc858c3c
Couragyn Chretien <couragyn@gmail.com>
2017-06-16 04:16:34 +0000

    made redirect for claim page if team is already claimed

--- db/dbFunc/getLeagueTeams.js
+++ db/dbFunc/getLeagueTeams.js
@@ -2,15 +2,17 @@
 module.exports = function getLeagueTeams(leagueID, knex) {
   return new Promise((resolve, reject) => {
     let userIDs = [];
-    knex.select('user_id').from('team')
+    let takenTeams = [];
+    knex.select('id', 'user_id').from('team')
       .where('league_id', '=', leagueID)
       .whereNotNull('user_id')
       .asCallback(function(err, rows) {
         if (err) return reject(err);
         for (let i = 0; i < rows.length; i++) {
-          userIDs.push(rows[i].user_id)
+          userIDs.push(rows[i].user_id);
+          takenTeams.push(rows[i].id);
         }
-        resolve(userIDs);
+        resolve({userIDs: userIDs, takenTeams: takenTeams});
       })
   })
 }
--- routes/league.js
+++ routes/league.js
@@ -89,9 +89,7 @@ module.exports = (knex) => {
           leagueDraftInfo.then(function(draftInfo) {
             let leagueTeams = getLeagueTeams(req.params.leagueID, knex);
             leagueTeams.then(function(teams) {
-              console.log(teams);
-              console.log(req.session.userID);
-              res.render("football/league/view", {userID: req.session.userID, username: req.session.username, draftData: draftInfo, leagueData: leagueData, leaguePositions: leaguePositions, teamData: teamData, takenTeams: teams});
+              res.render("football/league/view", {userID: req.session.userID, username: req.session.username, draftData: draftInfo, leagueData: leagueData, leaguePositions: leaguePositions, teamData: teamData, takenTeams: teams.userIDs});
             })
           })
         })
@@ -101,7 +99,15 @@ module.exports = (knex) => {
 
   router.get("/football/league/:leagueID/claim/:teamID", (req, res) => {
     if (req.session.userID) {
-      res.render('football/league/claim', {userID: req.session.userID, username: req.session.username, leagueID: req.params.leagueID, teamID: req.params.teamID});
+      let leagueTeams = getLeagueTeams(req.params.leagueID, knex);
+      leagueTeams.then(function(teams) {
+        let takenTeams = teams.takenTeams
+        if (!takenTeams.includes(parseInt(req.params.teamID))) {
+          res.render('football/league/claim', {userID: req.session.userID, username: req.session.username, leagueID: req.params.leagueID, teamID: req.params.teamID});
+        } else {
+          res.redirect('/football/league/'+req.params.leagueID);
+        }
+      })
     } else {
       res.redirect('/login');
     }
