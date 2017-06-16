d50c16ffd00fa61bcfd0c981d9cd5f44212540a5
Couragyn Chretien <couragyn@gmail.com>
2017-06-16 06:37:53 +0000

    added id to function call to also return id

--- db/dbFunc/getCommishTeams.js
+++ db/dbFunc/getCommishTeams.js
@@ -1,6 +1,6 @@
 module.exports = function getCommishTeams(commishID, knex) {
  return new Promise((resolve, reject) => {
-   knex.select('name', 'size', 'scoring', 'type').from('league')
+   knex.select('name', 'size', 'scoring', 'type', 'id').from('league')
     .where('commish_id', '=', commishID)
     .asCallback(function(err, rows) {
       if (err) return reject(err);
