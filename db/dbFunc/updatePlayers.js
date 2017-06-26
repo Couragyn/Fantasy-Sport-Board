ac9d493586cbee87a8919c743c22a9d50d7853a8
Couragyn Chretien <couragyn@gmail.com>
2017-06-26 07:39:09 +0000

    added comments for platers view

--- db/dbFunc/updatePlayers.js
+++ db/dbFunc/updatePlayers.js
@@ -1,9 +1,6 @@
 // Updates the player rankings
 module.exports = function updatePlayers(ranking, knex) {
   for (let i = 0; i < ranking.length; i++) {
-  console.log(ranking[i]);
-  console.log(i+1);
-
     knex('player')
       .where('id', '=', ranking[i])
       .update({position_adp: i+1})
